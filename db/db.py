26b244ac4f1b1f302438b1ca68c9bc2a456ee287
Yuriy Y <monkcrazy91@gmail.com>
2023-07-29 02:16:39 +0600

    Fix connect to database

--- db/db.py
+++ db/db.py
@@ -1,9 +1,10 @@
+import os
 import sqlite3 as sq
 
 
 class DataBase:
     def __init__(self):
-        self.con = sq.connect('acc.db')
+        self.con = sq.connect(os.path.join('db', 'acc.db'))
         self.cur = self.con.cursor()
 
     def create_tables(self):
