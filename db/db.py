c51de5b3cf35d8c5698ae66dc2bba1c4c288b276
Yuriy Y <monkcrazy91@gmail.com>
2023-07-29 15:52:31 +0600

    Delete a function for create tables

--- db/db.py
+++ db/db.py
@@ -8,15 +8,6 @@ class DataBase:
         self.con = sq.connect(os.path.join('db', 'acc.db'))
         self.cur = self.con.cursor()
 
-    def create_tables(self):
-        self.cur.execute("""CREATE TABLE IF NOT EXISTS category(uniqname PRIMARY KEY, name)""")
-        self.cur.execute("""CREATE TABLE IF NOT EXISTS expenses(
-        id INTEGER PRIMARY KEY,
-        cost INTEGER,
-        date DATETIME,
-        category_uniqname INTEGER,
-        FOREIGN KEY(category_uniqname) REFERENCES category(uniqname)""")
-
     def insert_expense(self, cost: int, category_uniqname: str):
         self.cur.execute("SELECT * FROM category WHERE uniqname = ?", (category_uniqname,))
         self.cur.execute("INSERT INTO expenses (cost, date, category_uniqname) VALUES (?, ?, ?)",
