4432fde73d4d4e11f8f093ba55674dbec5f52c50
Yuriy Y <monkcrazy91@gmail.com>
2023-07-29 18:05:58 +0600

    Fix function insert in db.py

--- db/db.py
+++ db/db.py
@@ -9,12 +9,14 @@ class DataBase:
         self.con = sq.connect(os.path.join('db', 'acc.db'))
         self.cur = self.con.cursor()
 
-    def insert_expense(self, cost: int, category_uniqname: str):
-        self.cur.execute("SELECT * FROM category WHERE uniqname = ?", (category_uniqname,))
-        self.cur.execute("INSERT INTO expenses (cost, date, category_uniqname) VALUES (?, ?, ?)",
-                         (cost, datetime.datetime.now(), category_uniqname))
+    def insert(self, cost: int, category_name: str):
+        """Writes the expense to the database"""
+        uniqname_object = self.cur.execute("SELECT uniqname FROM category WHERE name = ?", (category_name,))
+        uniqname = uniqname_object.fetchone()[0]
+        self.cur.execute("INSERT INTO expense (cost, date, category_uniqname) VALUES (?, ?, ?)",
+                         (cost, datetime.datetime.now(), uniqname))
         self.con.commit()
 
     def get_category_names(self) -> List[str]:
         names = self.cur.execute("SELECT name FROM category")
-        return names.fetchall()
+        return [name[0] for name in names]
