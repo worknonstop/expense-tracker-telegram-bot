feca32314306e1d50521ea96c1941b863b1f67ee
Yuriy Y <monkcrazy91@gmail.com>
2023-07-30 11:26:23 +0600

    Add function for get month expenses

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -24,11 +24,23 @@ async def get_day_expenses(message: types.Message):
     for q in query:
         reply_message += f"*{q[0]}:* {q[1]}\n"
 
-    total_costs = db.get_sql_day_sum_expenses().fetchone()[0]
+    amount = db.get_sql_day_sum_expenses().fetchone()[0]
     await message.answer("*Расходы за сегодня:*\n\n" + reply_message +
-                         f"\n*Итог:* {total_costs}", parse_mode="Markdown")
+                         f"\n*Итог:* {amount}", parse_mode="Markdown")
+
+
+async def get_month_expenses(message: types.Message):
+    query = db.get_sql_month_expenses().fetchall()
+    reply_message = ""
+    for q in query:
+        reply_message += f"*{q[0]}:* {q[1]}\n"
+
+    amount = sum(q[1] for q in query)
+    await message.answer("*Расходы за месяц:*\n\n" + reply_message +
+                         f"\n*Итог:* {amount}", parse_mode="Markdown")
 
 
 def register_handlers(dp: Dispatcher):
     dp.register_message_handler(add_expense, regexp=r'([\d ]+) (.*)')
     dp.register_message_handler(get_day_expenses, commands=['day'])
+    dp.register_message_handler(get_month_expenses, commands=['month'])
