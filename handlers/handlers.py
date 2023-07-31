bdb97ce601e6a8d87f298bdcf9c2b4ee61ab4c60
Yuriy Y <monkcrazy91@gmail.com>
2023-07-31 16:41:06 +0600

    Add auth for all handlers

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -1,12 +1,31 @@
 import re
+import os
+from functools import wraps
+
+import text
 from aiogram import Dispatcher
 from aiogram import types
 from db.db import DataBase
-import text
+from dotenv import load_dotenv
 
 db = DataBase()
 
+load_dotenv('.env')
+ADMIN_IDS = os.getenv("ADMIN_IDS")
 
+
+def auth(func):
+    @wraps(func)
+    async def wrapped(message: types.Message, *args, **kwargs):
+        user_id = message.from_user.id
+        if str(user_id) not in ADMIN_IDS:
+            await message.answer("Нет доступа.")
+            return
+        return await func(message, *args, **kwargs)
+    return wrapped
+
+
+@auth
 async def add_expense(message: types.Message):
     match_message = re.match(r'([\d ]+) (.*)', message.text)
     cost = int(match_message.group(1))
@@ -19,6 +38,7 @@ async def add_expense(message: types.Message):
     await message.answer("Такой категории не существует")
 
 
+@auth
 async def get_day_expenses(message: types.Message):
     query = db.get_sql_day_expenses().fetchall()
     reply_message = ""
@@ -30,6 +50,7 @@ async def get_day_expenses(message: types.Message):
                          f"\n*Итог:* {amount}", parse_mode="Markdown")
 
 
+@auth
 async def get_month_expenses(message: types.Message):
     query = db.get_sql_month_expenses().fetchall()
     reply_message = ""
@@ -41,6 +62,7 @@ async def get_month_expenses(message: types.Message):
                          f"\n*Итог:* {amount}", parse_mode="Markdown")
 
 
+@auth
 async def get_week_expenses(message: types.Message):
     query = db.get_sql_week_expenses().fetchall()
     reply_message = ""
@@ -52,17 +74,20 @@ async def get_week_expenses(message: types.Message):
                          f"\n*Итог:* {amount}", parse_mode="Markdown")
 
 
+@auth
 async def get_commands(message: types.Message):
     commands = text.commands
     await message.answer(commands, parse_mode="Markdown")
 
 
+@auth
 async def get_categories(message: types.Message):
     list_categories = db.get_list_categories()
     category_names = "\n".join(category for category in list_categories)
     await message.answer("Все категории расходов:\n" + category_names)
 
 
+@auth
 async def get_five_last(message: types.Message):
     query = db.get_sql_five_last().fetchall()
     five_last = ""
@@ -71,6 +96,7 @@ async def get_five_last(message: types.Message):
     await message.answer("Показать последние расходы:\n" + five_last, parse_mode="Markdown")
 
 
+@auth
 async def delete_entry(message: types.Message):
     number_from_message = int(message.text[4:])
     five_last = db.get_sql_five_last().fetchall()
@@ -86,6 +112,6 @@ def register_handlers(dp: Dispatcher):
     dp.register_message_handler(get_month_expenses, commands=['month'])
     dp.register_message_handler(get_week_expenses, commands=['week'])
     dp.register_message_handler(get_commands, commands=['start', 'help'])
-    dp.register_message_handler(get_categories, commands=['categories'])
+    dp.register_message_handler(get_categories, commands=['kinds'])
     dp.register_message_handler(get_five_last, commands=['last'])
     dp.register_message_handler(delete_entry, regexp=r'/del(\d+)')
